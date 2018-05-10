139f504f282c46f8d0a0dd3c676637bb819e6125
BasyninNikita <32190908+BasyninNikita@users.noreply.github.com>
2018-05-11 01:34:40 +0300

    Update graph.cpp

--- tests/graph.cpp
+++ tests/graph.cpp
@@ -6,9 +6,9 @@ TEST_CASE("just a test")
 {
   std::string in{
      		"0 1 0 0 \n"
-		"1 0 1 1 \n"
-		"0 0 0 0 \n"
-		"0 0 0 0 \n"
+		"0 0 1 0 \n"
+		"0 0 0 1 \n"
+		"1 0 0 0 \n"
    };
 graph_t graph(4);
 std::istringstream stream(in);
@@ -17,7 +17,7 @@ std::vector<unsigned> result;
 result = graph.dfs(1);
 std::ostringstream output;
 graph.print(output, result);
-std::string out = "2 1 3 4 ";
+std::string out = "2 3 4 1 ";
 REQUIRE( out == output.str() );
 }
 
