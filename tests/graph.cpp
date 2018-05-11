a8bc467c7420db77c59b0ae62828c0800d7b6eb3
BasyninNikita <32190908+BasyninNikita@users.noreply.github.com>
2018-05-11 11:45:41 +0300

    Update graph.cpp

--- tests/graph.cpp
+++ tests/graph.cpp
@@ -20,4 +20,43 @@ graph.print(output, result);
 std::string out = "2 3 4 1 ";
 REQUIRE( out == output.str() );
 }
-
+TEST_CASE("exept1")
+{
+	try{
+		std::string in{
+     		"0 2 0 0 \n"
+		"0 0 1 0 \n"
+		"0 0 0 1 \n"
+		"1 0 0 0 \n"
+   		};
+		graph_t graph(4);
+		std::istringstream stream(in);
+		graph.read(stream);
+	}
+	bool a=false;
+	catch (std::invalid_argument)
+	{
+		a = true;
+	}
+REQUIRE(a==true)
+}
+TEST_CASE("exept2")
+{
+	try{
+		std::string in{
+     		"0 1 0 0 \n"
+		"0 0 1 0 \n"
+		"0 0 1 1 \n"
+		"1 0 0 0 \n"
+   		};
+		graph_t graph(4);
+		std::istringstream stream(in);
+		graph.read(stream);
+	}
+	bool a=false;
+	catch (std::invalid_argument)
+	{
+		a = true;
+	}
+REQUIRE(a==true)
+}
